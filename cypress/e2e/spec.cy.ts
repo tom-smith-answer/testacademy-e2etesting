e4950c7de3686489c7a1c93b537506ac8b0d1872
Tom <tom.smith@answerdigital.com>
2024-05-01 14:04:05 +0100

    creates initial log in tests for correct and incorrect data

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -2,9 +2,36 @@ describe('Conduit test framework', () => {
 
     it('signs in with a valid email and password', () => {
         cy.visit('/')
+
         cy.get(':nth-child(2) > .nav-link')
         .click()
+
         cy.getByPlaceholder('Email')
-        .type('tom.smith@answer')
+        .type('tom.smith@answerdigital.com')
+        cy.getByPlaceholder('Password')
+        .type('password')
+        cy.get('.btn').click()
+
+        cy.url().should('eq', `${Cypress.config('baseUrl')}#/`)
     })
+
+    it('fails to sign in and displays an appropriate error message when given incorrect login details', () => {
+        cy.visit('/')
+
+        cy.get(':nth-child(2) > .nav-link')
+        .click()
+
+        cy.getByPlaceholder('Email')
+        .type('john.smith@answerdigital.com')
+        cy.getByPlaceholder('Password')
+        .type('passcode')
+        cy.get('.btn').click()
+
+        cy.url().should('eq', `${Cypress.config('baseUrl')}#/login`)
+        cy.get('.error-messages > li').should('have.text', 'email or password is invalid')
+        
+    })
+
+
+
 })
